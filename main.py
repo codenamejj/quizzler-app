4c4de10081d9e608c061ece2307ab30abf792986
John Maroa <112829127+codenamejj@users.noreply.github.com>
2023-04-05 11:56:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
-from ui import QuizInterface, user_answer
+from ui import QuizInterface
 
 
 question_bank = []
